d39d8a3162fee4b0f7cd89eca0c18c2b7fb08faa
Люция <lvaleeva87@gmail.com>
2022-03-07 19:34:53 +0500

    retreiving all data

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, HttpResponse
 
+from .models import Article
+
 
 def article_list(request):
-    article = 'Моя первая статья'
-    return render(request, 'articles.html', {'article': article})
+    article_list = Article.objects.all().order_by('-published')
+    return render(request, 'articles.html', {'article_list': article_list})
