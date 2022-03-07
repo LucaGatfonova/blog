f17bf6e5e7a30c6b45f8dd3ed6a3698b89e61f52
Люция <lvaleeva87@gmail.com>
2022-03-07 13:59:14 +0500

    added model Article,registration in Admin Panel

--- blog/articles/admin.py
+++ blog/articles/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article
+
+
+# admin.site.register(Article)
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'published', 'author')
+    date_hierarchy = 'published'
+    search_fields = ('title', 'description')
