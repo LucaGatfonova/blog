34717165c60511e541a6d894fefa4fe0467dbdfe
Люция <lvaleeva87@gmail.com>
2022-03-07 17:05:09 +0500

    added django templates

--- blog/articles/views.py
+++ blog/articles/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render, HttpResponse
 
 
 def article_list(request):
-    return HttpResponse('ghffkjfkf')
+    article = 'Моя первая статья'
+    return render(request, 'articles.html', {'article': article})
