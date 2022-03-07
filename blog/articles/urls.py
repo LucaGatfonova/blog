1a857f79ef9e97dc322acc65f3941e29be3b6d6c
Люция <lvaleeva87@gmail.com>
2022-03-07 21:42:35 +0500

    added article detail

--- blog/articles/urls.py
+++ blog/articles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import article_list
+from .views import article_list, article_details
 
 urlpatterns = [
     path('', article_list, name='article_list'),
+    path('articles/<slug:slug>/', article_details, name='article_details'),
 ]
--- blog/articles/views.py
+++ blog/articles/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, get_object_or_404
 
 from .models import Article
 
@@ -6,3 +6,8 @@ from .models import Article
 def article_list(request):
     article_list = Article.objects.all().order_by('-published')
     return render(request, 'articles.html', {'article_list': article_list})
+
+
+def article_details(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    return render(request, 'details.html', {'article': article})
