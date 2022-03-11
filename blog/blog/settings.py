5c69cf1486fc571edbead2f5d1b2b14987d49d9c
Люция <lvaleeva87@gmail.com>
2022-03-11 15:23:40 +0500

    built in login

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -115,6 +115,11 @@ USE_I18N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = 'article_list'
+LOGIN_URL = 'login'
+LOGOUT_URL = 'logout'
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
